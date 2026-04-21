5126cacc466600508cd5a33b53c6e135a484b229
Steve-Yang-KR <73861054+Steve-Yang-KR@users.noreply.github.com>
2026-04-21 12:50:39 +0900

    Update page.js

--- app/(main)/upload/page.js
+++ app/(main)/upload/page.js
@@ -3,22 +3,27 @@
 import { useState } from "react";
 import { storage } from "@/lib/firebaseClient";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { db } from "@/lib/firebase";
+import { db, auth } from "@/lib/firebase";
 import { addDoc, collection } from "firebase/firestore";
 
 export default function Upload() {
   const [file, setFile] = useState(null);
 
   const upload = async () => {
-    if (!file) return;
+    const user = auth.currentUser;
 
-    const storageRef = ref(storage, `videos/${file.name}`);
+    if (!user) {
+      alert("Not logged in");
+      return;
+    }
+
+    const storageRef = ref(storage, `videos/${user.uid}/${file.name}`);
     await uploadBytes(storageRef, file);
 
     const url = await getDownloadURL(storageRef);
 
     await addDoc(collection(db, "videos"), {
-      player_id: "player1",
+      player_id: user.uid, // 🔥 핵심 변경
       video_url: url,
       analyzed: false
     });
